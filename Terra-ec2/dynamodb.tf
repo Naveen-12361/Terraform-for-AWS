4776983c321e3cf0bc418b2b22340028f4350b53
Naveen-12361 <dnaveenkumar8073@gmail.com>
2026-06-25 20:40:32 +0530

    new

--- Terra-ec2/dynamodb.tf
+++ Terra-ec2/dynamodb.tf
@@ -9,3 +9,18 @@ resource "aws_dynamodb_table" "example_table" {
     type = "S"
   }
 }
+
+
+
+
+
+#remote backend
+#code should be added in terraform.tf
+
+
+backend "s3"{
+    bucket = "bucket_name"
+    key    = "path/to/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "example-dynamodb-table"
+}
