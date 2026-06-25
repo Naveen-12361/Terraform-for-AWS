51d04e9f353b2817184015b8f13b71efd943934d
Naveen-12361 <dnaveenkumar8073@gmail.com>
2026-06-25 12:25:25 +0530

    updated files

--- install_nginx.sh
+++ install_nginx.sh
@@ -6,3 +6,9 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 echo "<h1> nginx is installed and running </h1>" | sudo tee /var/www/html/index.html
+
+
+
+# need to add cmd in ec2 instance before volume is mounted
+
+user_data = file("install_nginx.sh")
