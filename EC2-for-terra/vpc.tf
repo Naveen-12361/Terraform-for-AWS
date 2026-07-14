5c4f8dbabc843c0260d27c0dc6a71f0a9ef4563b
Naveen-12361 <dnaveenkumar8073@gmail.com>
2026-07-14 21:27:25 +0530

    Add route table for public access in VPC
    Added a route table resource with a default route to the internet gateway.

--- EC2-for-terra/vpc.tf
+++ EC2-for-terra/vpc.tf
@@ -16,3 +16,16 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+
+
+
+# route table custom
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
